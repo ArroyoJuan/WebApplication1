3d2d71ea036ab3931bd8051d6d095fbc18343abf
Juank <Juank@DESKTOP-B4T6E7C>
2025-02-07 20:21:56 -0600

    Se crea modeloo y conexion con la base de datos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebAppFormMVC.Models;
+
 namespace WebApplication1
 {
     public class Program
@@ -8,6 +11,10 @@ namespace WebApplication1
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            // Configurar Entity Framework Core
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
